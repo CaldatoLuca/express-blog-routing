ea84d59611bd9d03361a672be6913c7fe3cf032e
CaldatoLuca <lucacaldato15@gmail.com>
2024-05-27 15:46:01 +0200

    feat: show con gestione di slug errato

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,27 +2,61 @@ const posts = require("../db");
 
 //Lista dei posts
 const index = (req, res) => {
-  let html = "<h1>Lista dei Posts - Index</h1> <ul>";
+  res.format({
+    html: () => {
+      let html = "<h1>Lista dei Posts - Index</h1> <ul>";
 
-  posts.map(
-    (p) =>
-      (html += `<li>
-  <h3>${p.title}</h3>
-  <img src="/imgs/posts/${p.image}" alt="${p.title}" width="100">
-  <div>${p.content}</div>
-  <h5>${p.tags.map((t) => `#${t}`).join(" ")}</h5>
-  </li>`)
-  );
+      posts.map(
+        (p) =>
+          (html += `<li>
+        <h3>${p.title}</h3>
+        <img src="/imgs/posts/${p.image}" alt="${p.title}" width="100">
+        <div>${p.content}</div>
+        <h5>${p.tags.map((t) => `#${t}`).join(" ")}</h5>
+        </li>`)
+      );
 
-  html += "</ul>";
+      html += "</ul>";
 
-  res.send(html);
+      res.send(html);
+    },
+    json: () => {
+      res.json({
+        posts: posts,
+        number: posts.length,
+      });
+    },
+  });
 };
 
 //Show del singolo post
 const show = (req, res) => {
   const slug = req.params.slug;
-  res.send(`Richiesta fatta per post ${slug}`);
+  const requestedPost = posts.find((p) => p.slug === slug);
+
+  //Controllo se requstedPost esiste
+  if (posts.includes(requestedPost)) {
+    res.format({
+      html: () => {
+        let html = `<h1>Richiesta fatta per post ${slug}</h1>`;
+        html += `<h3>${requestedPost.title}</h3>
+          <img src="/imgs/posts/${requestedPost.image}" alt="${
+          requestedPost.title
+        }" width="100">
+          <div>${requestedPost.content}</div>
+          <h5>${requestedPost.tags.map((t) => `#${t}`).join(" ")}</h5>`;
+        res.send(html);
+      },
+      json: () => {
+        res.json({
+          richiesta: `Richiesta fatta per post ${slug}`,
+          post: requestedPost,
+        });
+      },
+    });
+  } else {
+    res.status(404).send(`Post ${slug} non trovato`);
+  }
 };
 
 module.exports = {
